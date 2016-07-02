3e407e19c190e405efeca1bb67b0e7fb5f8ee0a9
JDSBCC <ei12159@fe.up.pt>
2016-07-02 15:43:14 +0100

    FloodFill is working

---
+++ Assets/Scripts/BuildField.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class BuildField : MonoBehaviour {
+
+    public GameObject block;
+    public GameObject hero;
+    private List<List<int>> field;//0-nothing...1-block...2-temp_block...3-hero....4-floodfill
+    private bool bridgeone = false, bridgetwo = false;
+    public int size_x, size_y;
+    private int free_field_size = 0, area_count = 0;
+
+    // Use this for initialization
+    void Start () {
+        free_field_size = (size_x-2) * (size_y-2);
+        field = new List<List<int>>();
+
+        //build field
+        for(int i=1; i<= size_x; i++){
+            field.Add(new List<int>());
+            for (int j = 1; j <= size_y; j++) {
+                field[i-1].Add(0);
+                if (i==1 || j==1 || i == size_x || j == size_y) {
+                    field[i-1][j-1]=1;
+                    Instantiate(block, new Vector3((i-12)*0.51f, (j-8)*0.51f,0), Quaternion.identity);
+                }
+            }
+        }
+
+        //init hero
+        field[0][size_y-1] = 3;
+        Instantiate(hero, new Vector3(-5.61f, 3.57f, -1), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateField();
+        justfortest();
+    }
+
+    public int getField(int i, int j)
+    {
+        return field[i][j];
+    }
+    public void setField(int i, int j, int value)
+    {
+        field[i][j] = value;
+    }
+
+    public void updateTryingBridge(int x, int y)
+    {
+        if (!bridgeone && field[x][y]==0) {
+            bridgeone = true;
+            Debug.Log("bridgeone = " + bridgeone);
+        } else if (bridgeone && !bridgetwo && field[x][y] == 1) {
+            bridgetwo = true;
+            Debug.Log("secod = " + bridgetwo);
+        }
+    }
+
+    private void floodFill(int x, int y, int target, int replace)
+    {
+        if (field[x][y]!=target) return;
+
+        field[x][y] = replace;
+
+        if (x > 0) floodFill(x - 1, y, target, replace);
+        if (x < size_x-1) floodFill(x + 1, y, target, replace);
+        if (y > 0) floodFill(x, y - 1, target, replace);
+        if (y < size_y-1) floodFill(x, y + 1, target, replace);
+    }
+
+    private List<int> getSidePoints()
+    {
+        List<int> points = new List<int>();
+        bool end = false;
+        for(int i = 0; i< size_x && !end; i++){
+            for (int j = 0; j < size_y && !end; j++) {
+                int x = i;
+                int y = j;
+                if (x > 0 && x < size_x - 1 && field[x-1][y]==0 && field[x+1][y]==0)
+                {
+                    points.Add(x - 1);
+                    points.Add(y);
+                    end = true;
+                } else if (y > 0 && y < size_y - 1 && field[x][y-1] == 0 && field[x][y+1] == 0){
+                    points.Add(x);
+                    points.Add(y - 1);
+                    end = true;
+                }
+            }
+        }
+
+        return points;
+    }
+
+    private int getLowestArea()
+    { //retorna o valor da área mais pequena - 0 ou 4 (preencher os 2 depois)
+        int count = 0;
+
+        for (int i = 0; i < size_x; i++) {
+            for (int j = 0; j < size_y; j++)
+            {
+                if (field[i][j] == 4) count++;
+            }
+        }
+        if (free_field_size - count > 0) return 4;
+        return 0;
+    }
+
+    private void fillArea(int value, int replace)
+    {
+        for (int i = 0; i < size_x; i++)
+        {
+            for (int j = 0; j < size_y; j++)
+            {
+                if (field[i][j] == value) field[i][j] = replace;
+            }
+        }
+    }
+
+    public void updateField()
+    {
+        if(bridgeone && bridgetwo)
+        {
+            bridgeone = false;
+            bridgetwo = false;
+
+            List<int> side_points = getSidePoints();
+            floodFill(side_points[0], side_points[1], 0, 4);
+            int value = getLowestArea();
+            fillArea(value,1);
+            fillArea(2, 1);
+        }
+    }
+
+    public void justfortest()
+    {
+        string test = "";
+        for (int j = field[0].Count-1; j >=0 ; j--)
+        {
+            for (int i = 0; i < field.Count; i++) {
+                test += field[i][j] + " ";
+            }
+            test += "\n";
+        }
+        Debug.Log(test);
+    }
+}
---
+++ Assets/Scripts/SwipeDetector.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetector : MonoBehaviour
+{
+    private float fingerStartTime = 0.0f;
+    private Vector2 fingerStartPos = Vector2.zero;
+    private bool isSwipe = false;
+    private float minSwipeDist = 50.0f;
+    private float maxSwipeTime = 0.5f;
+
+    private float left=0, right = 0, up=0.51f, down=0;
+    private Vector3 position;
+    public GameObject block;
+    private BuildField field;
+
+    private void Start()
+    {
+        field = GameObject.FindGameObjectWithTag("field").GetComponent<BuildField>();
+        position = this.transform.position;
+        Invoke("move", 0.2f);
+    }
+    
+    private void Update() {
+
+        position = this.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            changePosition(0.51f, 0, 0, 0);
+            //transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            changePosition(0, 0.51f, 0, 0);
+            //transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            changePosition(0, 0, 0.51f, 0);
+            //transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            changePosition(0, 0, 0, 0.51f);
+            //transform.rotation = Quaternion.AngleAxis(270, Vector3.forward);
+        }
+
+        if (Input.touchCount > 0) {
+            foreach (Touch touch in Input.touches) {
+                switch (touch.phase){
+                    case TouchPhase.Began:
+                        /* this is a new touch */
+                        isSwipe = true;
+                        fingerStartTime = Time.time;
+                        fingerStartPos = touch.position;
+                        break;
+                    case TouchPhase.Canceled:
+                        /* The touch is being canceled */
+                        isSwipe = false;
+                        break;
+                    case TouchPhase.Ended:
+
+                        float gestureTime = Time.time - fingerStartTime;
+                        float gestureDist = (touch.position - fingerStartPos).magnitude;
+
+                        if (isSwipe && gestureTime < maxSwipeTime && gestureDist > minSwipeDist){
+                            Vector2 direction = touch.position - fingerStartPos;
+                            Vector2 swipeType = Vector2.zero;
+
+                            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {// the swipe is horizontal:
+                                swipeType = Vector2.right * Mathf.Sign(direction.x);
+                            } else { // the swipe is vertical:
+                                swipeType = Vector2.up * Mathf.Sign(direction.y);
+                            }
+
+                            if (swipeType.x != 0.0f) {
+                                if (swipeType.x > 0.0f) {//right
+                                    changePosition(0, 0.51f, 0, 0);
+                                    //transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+                                } else {//lefy
+                                    changePosition(0.51f, 0, 0, 0);
+                                    //transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+                                }
+                            }
+
+                            if (swipeType.y != 0.0f) {
+                                if (swipeType.y > 0.0f) {//up
+                                    changePosition(0, 0, 0.51f, 0);
+                                    //transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+                                } else {//down
+                                    changePosition(0, 0, 0, 0.51f);
+                                    //transform.rotation = Quaternion.AngleAxis(270, Vector3.forward);
+                                }
+                            }
+
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+
+    private void move()
+    {
+        field.setField((int)(Math.Ceiling(position.x / 0.51f) + 12 - 1), (int)(Math.Ceiling(position.y / 0.51f) + 8 - 1), 2);
+        if (position.x - left > -6 && position.x + right < 6 && position.y + up < 3.6f && position.y - down > -3.6f) {
+            position.x -= left;
+            position.x += right;
+            position.y += up;
+            position.y -= down;
+            position.z = -1;
+        }
+        this.transform.position = position;
+        field.updateTryingBridge((int)(Math.Ceiling(position.x / 0.51f) + 12 - 1), (int)(Math.Ceiling(position.y / 0.51f) + 8 - 1));
+        field.setField((int)(Math.Ceiling(position.x / 0.51f) + 12 - 1), (int)(Math.Ceiling(position.y / 0.51f) + 8 - 1), 3);
+        Invoke("move", 0.2f);
+    }
+
+    private void changePosition(float left, float right, float up, float down) {
+        this.left = left;
+        this.right = right;
+        this.up = up;
+        this.down = down;
+    }
+}
